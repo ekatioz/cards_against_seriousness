e196ff0c691f370133a7eb48adb059fe48f2ba59
ekatioz <ekatioz@gmail.com>
2019-03-03 15:57:52 +0100

    bugfix button

--- src/client/ui/ProceedButton.js
+++ src/client/ui/ProceedButton.js
@@ -5,7 +5,7 @@ export class ProceedButton extends UiElement{
     constructor(caption, action) {
         super();
         this.addClass('proceed-button');
-        this.element.innerHTML = `<div class="button-text">${caption}</div>`;
+        this.element.innerText = caption;
         this.element.addEventListener('click',action);
     }
 }
