487253364660a1b034b966ccdc39575d54d3d00b
ekatioz <ekatioz@gmail.com>
2019-03-04 09:32:32 +0100

    Login Cookie #8

--- src/client/ui/Login.js
+++ src/client/ui/Login.js
@@ -2,6 +2,7 @@ import { FullscreenElement } from "./FullscreenElement";
 import { ProceedButton } from "./ProceedButton";
 import getName from "../../resources/names";
 
+const userCookie = 'username';
 export class Login extends FullscreenElement {
 
     constructor(loginCallback) {
@@ -9,11 +10,36 @@ export class Login extends FullscreenElement {
         this.addClass('login');
         const username = document.createElement('input');
         username.className = 'username-input';
-        username.value = getName();
+        username.value = getCookie(userCookie) || getName();
         this.addDomElement(username);
-        this.addUiElement(new ProceedButton('Login', () =>
-            username.value && username.value !== ''
-                ? loginCallback(username.value)
-                : {}));
+        this.addUiElement(new ProceedButton('Login', () => {
+            if (username.value && username.value !== '') {
+                setCookie(userCookie, username.value, 60);
+                loginCallback(username.value);
+            }
+        }));
     }
+}
+
+function setCookie(cname, cvalue, exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+    var expires = "expires=" + d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+function getCookie(cname) {
+    var name = cname + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for (var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return;
 }
