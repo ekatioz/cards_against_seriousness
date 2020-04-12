db98bfe846a392599c325f9e6de1d616bd7ac298
ekatioz <ekatioz@gmail.com>
2020-04-12 12:55:56 +0200

    revert fix

--- src/client/ui/Login.js
+++ src/client/ui/Login.js
@@ -2,6 +2,8 @@ import getName from "../../resources/names";
 
 import { LitElement, html, css, unsafeCSS } from "lit-element";
 
+import { getCookie, setCookie } from "../utils";
+
 import store from "../store/store";
 
 import fullscreen from "../../resources/fullscreen.comp.css";
@@ -12,12 +14,20 @@ import "./ProceedButton";
 
 import "./Footer";
 
+
+
+const userCookie = "username";
+
 class Login extends LitElement {
+
   static get styles() {
+
     return css`
+
       ${unsafeCSS(fullscreen)}
 
       .username-input {
+
         background-color: #222;
 
         color: #eee;
@@ -35,54 +45,90 @@ class Login extends LitElement {
         width: 100%;
 
         margin-bottom: 10vh;
+
       }
+
+
 
       .username-input:focus {
+
         box-shadow: 0px 2px 0px 0px #ff9021;
+
       }
+
     `;
+
   }
 
+
+
   onLogin() {
+
     const username = store.getState().username;
 
     if (username && username !== "") {
-      // setCookie(userCookie, username, 60);
+
+      setCookie(userCookie, username, 60);
 
       store.dispatch(login());
+
     }
+
   }
+
+
 
   onUsernameChanged(e) {
-    const username = e.path[0].value;
+
+    const username = e.target.value;
 
     store.dispatch(updateUsername(username));
+
   }
 
+
+
   render() {
+
     return html`
+
       <input
+
         class="username-input"
+
         @change="${this.onUsernameChanged}"
+
         value="${LoadUsername()}"
+
       />
 
       <proceed-button
+
         action="Login"
+
         @proceed="${this.onLogin}"
+
       ></proceed-button>
 
       <cards-footer></cards-footer>
+
     `;
+
   }
+
 }
+
+
 
 customElements.define("user-login", Login);
 
 function LoadUsername() {
-  const username = getName();
+
+  const username = getCookie(userCookie) || getName();
 
   store.dispatch(updateUsername(username));
 
   return username;
+
 }
+
